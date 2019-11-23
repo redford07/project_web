a7b1c7d524ce6b878a135173b8281634f8747c4e
“Themotif” <thekim07@gmail.com>
2019-11-23 16:29:28 +0900

    upload

--- website/db/db_info.js
+++ website/db/db_info.js
@@ -1,24 +1,24 @@
 module.exports = (function () {
   return {
     local: { // localhost
-      host: '54.201.86.156',
+      host: 'localhost',
       port: '3306',
       user: 'root',
-      password: 'nm9410',
+      password: 'themotif0518',
       database: 'bakze'
     },
     real: { // real server db info
-      host: '54.201.86.156',
+      host: 'localhost',
       port: '3306',
       user: 'root',
-      password: 'nm9410',
+      password: 'themotif0518',
       database: 'bakze'
     },
     dev: { // dev server db info
-      host: '54.201.86.156',
+      host: 'localhost',
       port: '3306',
       user: 'root',
-      password: 'nm9410',
+      password: 'themotif0518',
       database: 'bakze'
     }
   }
