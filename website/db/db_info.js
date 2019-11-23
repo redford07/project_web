7f244211f950aaa3b2c5d9bf2da35f8793594a7a
“Themotif” <thekim07@gmail.com>
2019-11-23 16:23:50 +0900

    123

---
+++ website/db/db_info.js
@@ -0,0 +1,25 @@
+module.exports = (function () {
+  return {
+    local: { // localhost
+      host: '54.201.86.156',
+      port: '3306',
+      user: 'root',
+      password: 'nm9410',
+      database: 'bakze'
+    },
+    real: { // real server db info
+      host: '54.201.86.156',
+      port: '3306',
+      user: 'root',
+      password: 'nm9410',
+      database: 'bakze'
+    },
+    dev: { // dev server db info
+      host: '54.201.86.156',
+      port: '3306',
+      user: 'root',
+      password: 'nm9410',
+      database: 'bakze'
+    }
+  }
+})();
---
+++ website/start.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <unistd.h>
+
+main(){
+system("nodemon start");
+
+}
